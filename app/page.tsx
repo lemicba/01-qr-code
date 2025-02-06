632baa19c5ff2b42d4985a858e58122eb897e40d
Emiliano Ceballos <lemicba@gmail.com>
2025-02-06 11:53:44 -0300

    fix: shadow fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center overflow-x-hidden font-[family-name:var(--font-outfit-mono)]">
+    <div className="w-full min-h-screen flex flex-col justify-center overflow-x-hidden font-[family-name:var(--font-outfit-mono)] bg-slate-300">
       <main className="flex justify-center">
-        <div className="bg-white w-[320px] h-[499px] rounded-[20px] px-4">
+        <div className="bg-white w-[320px] h-[499px] rounded-[20px] px-4 shadow-xl shadow-black/5">
           <Image
             src={qrPic}
             width={288}
@@ -13,9 +13,9 @@ export default function Home() {
             alt="Qr Image"
             className="rounded-[10px] mt-4"
           />
-          <h2 className="text-slate-900 text-[22px] font-bold leading-[120%] tracking-normal text-center mt-6">
+          <h1 className="text-slate-900 text-[22px] font-bold leading-[120%] tracking-normal text-center mt-6">
             Improve your front-end skills by building projects
-          </h2>
+          </h1>
           <p className="text-slate-500 text-[15px] leading-[140%] tracking-[0.2px] text-center mt-4">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
